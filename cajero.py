1fa780c8be0a769d654edb7db55f5c7f6810a459
yateAndres <yateloaizaandresfelipe47@gmail.com>
2026-02-12 16:30:36 -0500

    modificacion en el menu

--- cajero.py
+++ cajero.py
@@ -15,11 +15,43 @@ def retirar():
 def depositar():
     pass
 
-
-# MENU PRINCIPAL
-def menu():
+# mostrar historial
+def mostrar_historial():
     pass
 
+# menu
+def menu(usuario):
+    while True:
+        print("\n===== CAJERO AUTOMATICO =====")
+        print("1. Consultar saldo")
+        print("2. Retirar dinero")
+        print("3. Depositar dinero")
+        print("4. Ver historial")
+        print("5. Salir")
 
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            consultar_saldo(usuario)
+        elif opcion == "2":
+            retirar(usuario)
+        elif opcion == "3":
+            depositar(usuario)
+        elif opcion == "4":
+            mostrar_historial()
+        elif opcion == "5":
+            print("Gracias por usar el cajero")
+            break
+        else:
+            print("Opción inválida")
+
+
+# ================== MAIN ==================
 def main():
-    pass
+    usuario = login()
+    if usuario:
+        menu(usuario)
+
+
+if __name__ == "__main__":
+    main()
