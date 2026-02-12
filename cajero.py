a252ef9baa45f1efd7128ea896a7603bd80115ba
yateAndres <yateloaizaandresfelipe47@gmail.com>
2026-02-12 16:11:44 -0500

    Segundo commit

---
+++ cajero.py
@@ -0,0 +1,25 @@
+# AUTENTICACION
+def login():
+    pass
+
+
+# CONSULTA DE SALDO
+def consultar_saldo():
+    pass
+
+
+# TRANSACCIONES
+def retirar():
+    pass
+
+def depositar():
+    pass
+
+
+# MENU PRINCIPAL
+def menu():
+    pass
+
+
+def main():
+    pass
