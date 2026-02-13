52e7a49818a43d104e5c40d2163f3f9876c6b78e
yolima rodriguez <njimenagarcia021@gmail.com>
2026-02-12 19:08:42 -0500

    cambios saldo

--- cajero.py
+++ cajero.py
@@ -1,27 +1,43 @@
 # AUTENTICACION
 def login():
-    pass
+    # Usuario de prueba
+    usuario = {
+        "nombre": "Nicole",
+        "saldo": 1000.0
+    }
+    return usuario
 
 
 # CONSULTA DE SALDO
-def consultar_saldo():
-    pass
+def consultar_saldo(usuario):
+    """
+    Muestra el saldo actual del usuario en pantalla.
+    """
+    print("\n" + "="*30)
+    print(f"TITULAR: {usuario['nombre']}")
+    print(f"SALDO DISPONIBLE: ${usuario['saldo']:.2f}")
+    print("="*30)
 
 
 # TRANSACCIONES
-def retirar():
+def retirar(usuario):
     pass
 
-def depositar():
+
+def depositar(usuario):
     pass
+
 
 # mostrar historial
 def mostrar_historial():
     pass
 
+
 # menu
 def menu(usuario):
+
     while True:
+
         print("\n===== CAJERO AUTOMATICO =====")
         print("1. Consultar saldo")
         print("2. Retirar dinero")
@@ -33,25 +49,33 @@ def menu(usuario):
 
         if opcion == "1":
             consultar_saldo(usuario)
+
         elif opcion == "2":
             retirar(usuario)
+
         elif opcion == "3":
             depositar(usuario)
+
         elif opcion == "4":
             mostrar_historial()
+
         elif opcion == "5":
             print("Gracias por usar el cajero")
             break
+
         else:
             print("Opción inválida")
 
 
-# ================== MAIN ==================
+# ================= MAIN =================
+
 def main():
+
     usuario = login()
+
     if usuario:
         menu(usuario)
 
 
-if __name__ == "__main__":
+if _s_name__ == "__main__":
     main()
